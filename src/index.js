292c1a6367109bf6b8ab7ed73a7864ccc870de8f
Colin J Riddell <colinjohnriddell@gmail.com>
2017-07-10 23:18:36 +0100

    very basic app, no redux

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBrER2e35hT3qROd1gwQybHqcDGOlJ4ihI';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={ this.state.videos } />
+      </div>
+    );
+  }
+}
+// Take component's HTML and put it into the DOM
+//ReactDOM.render(<App />, document.getElementById('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
